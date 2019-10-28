ecd5ff934420d5482d205dd89a4731a3a15344b7
Wendy Garcia <wendy.fern84@gmail.com>
2019-10-28 14:09:31 -0700

    Updates

---
+++ precache-manifest.0c056b5d8f052cb02ff1b8076ec4dbf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9a8855b548a5d1f0ffe36dfe3ae066",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a23c68b551dce3e14a8d",
+    "url": "/clicky-game/static/css/main.2b9aecab.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/clicky-game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "a23c68b551dce3e14a8d",
+    "url": "/clicky-game/static/js/main.7a0e8a1a.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  },
+  {
+    "revision": "bd4cfdaf9b8c6720418d94aef87483b4",
+    "url": "/clicky-game/static/media/background.bd4cfdaf.jpg"
+  }
+]);
